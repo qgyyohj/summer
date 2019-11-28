5191238f1c1983d632cc0403f5549a88f5a36587
zhangyuzhuo <1415068436@qq.com>
2019-11-28 15:39:07 +0800

    一次修改

--- summer-business/summer-business-core/src/main/java/com/summer/business/core/bean/Employee.java
+++ summer-business/summer-business-core/src/main/java/com/summer/business/core/bean/Employee.java
@@ -1,37 +1,15 @@
 package com.summer.business.core.bean;
 
+import lombok.Data;
 /**
  * @author yuzhuozhang
  */
+@Data
 public class Employee {
 
     private int id;
     private String name;
     private int age;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 
     @Override
     public String toString(){
